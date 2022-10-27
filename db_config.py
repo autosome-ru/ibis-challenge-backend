c29eb1794c3b34b57161dbeca104f2322eb4f31f
Nikita Gryzunov <nikgr127@gmail.com>
2022-10-27 22:09:41 +0300

    Add requirements and changes in db config

--- db_config.py
+++ db_config.py
@@ -5,7 +5,8 @@ import pathlib
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(pathlib.Path(__file__).parent.absolute(), 'COMPETITION.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(pathlib.Path(__file__).parent.absolute(),
+                                                          'IBIS_COMPETITION.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     RESTPLUS_VALIDATE = True
     LOGGER_LEVEL = 'DEBUG'
