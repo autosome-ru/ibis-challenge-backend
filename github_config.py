eb8ecde70fb5b2ef2023ade3d4f8cabf7e620f1d
Nikita Gryzunov <nikgr127@gmail.com>
2022-11-04 01:43:59 +0300

    Petson deployment

--- github_config.py
+++ github_config.py
@@ -1,7 +1,7 @@
 github_config = {
     "client_id": "e853dfc011084647e1aa",
     "client_secret": "a9399a5ce1df49358e6eadba0b7bb40ea0911cd0",
-    "redirect_uri": "http://localhost:4200/callback",
+    "redirect_uri": "http://petson.autosome.ru/callback",
     "github_access_token_api_url": "https://github.com/login/oauth/access_token",
     "github_user_api_url": "https://api.github.com/user"
 }
---
+++ github_config_dev.py
@@ -0,0 +1,7 @@
+github_config = {
+    "client_id": "06d6690509176716f42e",
+    "client_secret": "d597a1811b2ab9b80cb2acdae30fa35b073ca8d7",
+    "redirect_uri": "http://localhost:4200/callback",
+    "github_access_token_api_url": "https://github.com/login/oauth/access_token",
+    "github_user_api_url": "https://api.github.com/user"
+}
